30c9bbd9a1bfa029e20873b37d8233ee8ea1b087
Efko77 <matavimai.evaldas@gmail.com>
2018-07-26 11:06:09 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,85 @@
+package com.company;
+
+import com.company.maker.CoffeMaker;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    /*Papildyti kavos aparata šiuo funkcionalumu:
+    - vartojas pats įveda kavos pavadinimą
+    - kai tikrinama ar aparatas pasiruošęs, pranešti jei kažko trūksta ir ar reikia valyti įskaitant ir prieš gaminant kavą
+    - visur naudoti string builder
+    - prideti "ASCII art" kavos puodeliams, naudoti string builder
+    - pildant aparatą iš vartotojo pasiimti kiekius kiek papildys*/
+
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.dirbam();
+    }
+
+    private void showMenu1() {
+        System.out.println("Iveskite kavos pavadinima is pateikto meniu");
+        System.out.println("black");
+        System.out.println("latte");
+        System.out.println("esspresso");
+    }
+
+    private void showMenu2() {
+        System.out.println("0 - baigti darba");
+        System.out.println("1 - rodyti aparato busena");
+        System.out.println("2 - plauti aparata");
+        System.out.println("3 - pildyti produktus");
+    }
+
+    private void dirbam() {
+        CoffeMaker aparatas = new CoffeMaker();
+        Scanner sc = new Scanner(System.in);
+        boolean arTesti = true;
+        do {
+            showMenu1();
+
+            String pasirinkimas1 = sc.next();
+
+            switch (pasirinkimas1) {
+                if (aparatas.isReady()) {
+
+                    case "black":
+                        aparatas.makeCoffe("black");
+                        break;
+                    case "latte":
+                        aparatas.makeCoffe("latte");
+                        break;
+                    case "espreso":
+                        aparatas.makeCoffe("espresso");
+                        break;
+                }
+            }
+            showMenu2();
+            int pasirinkimas2 = sc.nextInt();
+            switch (pasirinkimas2) {
+                case 0:
+                    arTesti = false;
+                    break;
+                case 1:
+                    aparatas.showStatus();
+                    break;
+                case 2:
+                    aparatas.washMaker();
+                    break;
+                case 3:
+                    System.out.println("iveskite kiek reikia vandens");
+                    float water = sc.nextFloat();
+                    System.out.println("iveskite kiek reikia cukraus");
+                    float sugar = sc.nextFloat();
+                    System.out.println("iveskite kiek reikia pupeliu");
+                    float beens = sc.nextFloat();
+                    aparatas.fillProducts(water, sugar, beens);
+                    break;
+
+            }
+
+        } while (arTesti);
+    }
+}
---
+++ src/com/company/maker/CoffeMaker.java
@@ -0,0 +1,150 @@
+package com.company.maker;
+
+public class CoffeMaker {
+
+
+    public static final int MAX_USES = 5;
+    private float sugarAmount;
+    private float waterAmount;
+    private float beansAmount;
+    private int useCount;
+
+
+    public CoffeMaker() {
+    }
+
+    public CoffeMaker(float water, float sugar, float beans) {
+        waterAmount = water;
+        sugarAmount = sugar;
+        beansAmount = beans;
+    }
+
+
+    public void fillProducts(float water, float sugar, float beans) {
+        waterAmount += water;
+        sugarAmount += sugar;
+        beansAmount += beans;
+    }
+
+
+    public void makeCoffe(String coffeType) {
+        if (!isReady()) return;
+        StringBuilder builder = new StringBuilder("");
+
+        switch (coffeType.toLowerCase()) {
+            case "black":
+                sugarAmount -= 10;
+                beansAmount -= 20;
+                waterAmount -= 0.25f;
+                builder.append("                        (\n" +
+                        "                          )     (\n" +
+                        "                   ___...(-------)-....___\n" +
+                        "               .-\"\"       )    (          \"\"-.\n" +
+                        "         .-'``'|-._             )         _.-|\n" +
+                        "        /  .--.|   `\"\"---...........---\"\"`   |\n" +
+                        "       /  /    |                             |\n" +
+                        "       |  |    |                             |\n" +
+                        "        \\  \\   |                             |\n" +
+                        "         `\\ `\\ |                             |\n" +
+                        "           `\\ `|                             |\n" +
+                        "           _/ /\\                             /\n" +
+                        "          (__/  \\                           /\n" +
+                        "       _..---\"\"` \\                         /`\"\"---.._\n" +
+                        "    .-'           \\                       /          '-.\n" +
+                        "   :               `-.__             __.-'              :\n" +
+                        "   :                  ) \"\"---...---\"\" (                 :\n" +
+                        "    '._               `\"--...___...--\"`              _.'\n" +
+                        "  jgs \\\"\"--..__                              __..--\"\"/\n" +
+                        "       '._     \"\"\"----.....______.....----\"\"\"     _.'\n" +
+                        "          `\"\"--..,,_____            _____,,..--\"\"`\n" +
+                        "                        `\"\"\"----\"\"\"`");
+                break;
+            case "latte":
+                sugarAmount -= 20;
+                beansAmount -= 40;
+                waterAmount -= 0.33f;
+                System.out.println("");
+
+                break;
+            case "esspresso":
+                sugarAmount -= 10;
+                beansAmount -= 40;
+                waterAmount -= 0.1f;
+                break;
+        }
+        useCount++;
+        sugarAmount = Math.max(sugarAmount, 0);
+        waterAmount = Math.max(waterAmount, 0);
+        beansAmount = Math.max(beansAmount, 0);
+
+    }
+
+
+    public void washMaker() {
+        this.useCount = 0;
+    }
+
+
+    public boolean isReady() {
+
+        boolean result = true;
+        if (useCount >= MAX_USES) {
+            result = false;
+            System.out.println("Truksta pupeliu");
+        }
+        if (waterAmount <= 0) return false;
+        if (sugarAmount <= 0) return false;
+        if (beansAmount <= 0) return false;
+
+        return true;
+    }
+
+
+    public void showProducts() {
+        System.out.println("=========Product status=========");
+        System.out.println("Sugar amount left:" + sugarAmount);
+        System.out.println("Water amount left:" + waterAmount);
+        System.out.println("Beans amount left:" + beansAmount);
+
+    }
+
+
+    public void showStatus() {
+        System.out.println("=========Maker status============");
+        System.out.println("Usages left:" + (MAX_USES - this.useCount));
+        System.out.println("is machine ready:" + isReady());
+        this.showProducts();
+    }
+
+
+    public float getWater() {
+        return this.waterAmount;
+    }
+
+
+    public void setWater(float value) {
+        this.waterAmount = value;
+    }
+
+
+    public float getBeans() {
+        return this.beansAmount;
+    }
+
+
+    public void setBeans(float value) {
+        this.beansAmount = value;
+    }
+
+
+    public float getSugar() {
+        return this.sugarAmount;
+    }
+
+
+    public void setSugar(float value) {
+        this.sugarAmount = value;
+    }
+
+
+}
